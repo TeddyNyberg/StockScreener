87c0df253c42f11174b49e6db49333ebf3813dfd
Ted Nyberg <tnyberg55@gmail.com>
2025-11-05 12:35:32 -0500

    renaming

--- app/ml_logic/model_loader.py
+++ app/ml_logic/model_loader.py
@@ -5,27 +5,27 @@ import torch
 from settings import *
 
 
-s3_client = boto3.client(
+S3_CLIENT = boto3.client(
         's3',
         aws_access_key_id = AWS_ACC_KEY_ID,
         aws_secret_access_key= AWS_SCR_ACC_KEY
     )
 
 
-_MODEL_STATE_DICT = None
-_CONFIG = None
+MODEL_STATE_DICT = None
+CONFIG = None
 def load_model_artifacts():
-    global _MODEL_STATE_DICT, _CONFIG
+    global MODEL_STATE_DICT, CONFIG
 
-    if _MODEL_STATE_DICT is not None and _CONFIG is not None:
+    if MODEL_STATE_DICT is not None and CONFIG is not None:
         print("Using cached model artifacts.")
-        return _MODEL_STATE_DICT, _CONFIG
+        return MODEL_STATE_DICT, CONFIG
 
     print("Loading model artifacts from S3...")
 
     model_buffer = io.BytesIO()
 
-    s3_client.download_fileobj("sagemaker-us-east-1-307926602475", MODEL_ARTIFACTS_PREFIX, model_buffer)
+    S3_CLIENT.download_fileobj("sagemaker-us-east-1-307926602475", MODEL_ARTIFACTS_PREFIX, model_buffer)
     model_buffer.seek(0)
     print("Downloaded entire model archive to memory.")
 
@@ -33,11 +33,11 @@ def load_model_artifacts():
         with tar.extractfile('model.pth') as f:
             checkpoint = torch.load(io.BytesIO(f.read()))
 
-    _MODEL_STATE_DICT = checkpoint.get("model_state")
-    _CONFIG = checkpoint.get("config")
+    MODEL_STATE_DICT = checkpoint.get("model_state")
+    CONFIG = checkpoint.get("config")
 
-    if _MODEL_STATE_DICT is None:
+    if MODEL_STATE_DICT is None:
         raise KeyError("Could not find 'model_state' key in the loaded dictionary.")
 
     print("Model state dict and config loaded successfully.")
-    return _MODEL_STATE_DICT, _CONFIG
+    return MODEL_STATE_DICT, CONFIG
